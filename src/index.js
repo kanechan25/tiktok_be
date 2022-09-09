8879551c9bf2864c2d7c61a498cde06ce3154a01
kanechan25 <kanechan.civilengineer@gmail.com>
2022-09-09 09:30:58 +0700

    adjust-fe

--- src/index.js
+++ src/index.js
@@ -23,13 +23,13 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', process.env.URL_FED);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', process.env.URL_FED);
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 
 app.engine(
     'hbs',
